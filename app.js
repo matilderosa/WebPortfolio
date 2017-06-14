9b01ec4581f3ccdc12e698d9272adcb05887f07a
Matilde Rosa <matilderosa3@hotmail.com>
2017-06-14 01:46:19 +0100

    correct error in PORT

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.ID, function(){
     console.log("server has started");
 });
