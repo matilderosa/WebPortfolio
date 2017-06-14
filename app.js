58f22d02cbfa48f7b9d4127004cbb76b3a3493d9
Matilde Rosa <matilderosa3@hotmail.com>
2017-06-14 12:20:09 +0100

    solve issue with cloudinary

--- app.js
+++ app.js
@@ -3,28 +3,15 @@ var express               = require("express"),
     app                   = express();
 
 
-
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-
 //Cloudinary Config
 cloudinary.config({
-  cloud_name: 'sample',
-  api_key: '874837483274837',
+  cloud_name: 'mrosa',
+  api_key: '468163595992795',
   api_secret: process.env.CLOUDINARY_URL
 });
-
-cloudinary.uploader.upload("/assets/Untitled-9.jpg", function(result) {
-  console.log(result)
-});
-cloudinary.uploader.upload("/assets/UrbanFarmed.jpg", function(result) {
-  console.log(result)
-});
-cloudinary.uploader.upload("/assets/YelpCamp.jpg", function(result) {
-  console.log(result)
-});
-
 
 
 
