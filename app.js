5f98f95285ef309b0a356cf1b1cd86659df21178
Matilde Rosa <matilderosa3@hotmail.com>
2017-06-14 11:20:30 +0100

    include cloudinary

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var express               = require("express"),
+    cloudinary            = require('cloudinary');
     app                   = express();
+
 
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+
+//Cloudinary Config
+cloudinary.config({
+  cloud_name: 'sample',
+  api_key: '874837483274837',
+  api_secret: process.env.CLOUDINARY_URL
+});
+
+cloudinary.uploader.upload("/assets/Untitled-9.jpg", function(result) {
+  console.log(result)
+});
+cloudinary.uploader.upload("/assets/UrbanFarmed.jpg", function(result) {
+  console.log(result)
+});
+cloudinary.uploader.upload("/assets/YelpCamp.jpg", function(result) {
+  console.log(result)
+});
+
+
+
 
 // Root Route
 app.get("/", function(req, res){
